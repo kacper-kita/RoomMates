aebab69413b913166dfeb1a661110145836bfa5d
KacperK <kkita.contact@gmail.com>
2020-01-26 21:16:55 +0100

    Initial Commit

---
+++ RoomMates/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoomMates
+//
+//  Created by Kacper Kita on 26/01/2020.
+//  Copyright © 2020 Kacper Kita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
